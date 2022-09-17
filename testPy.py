9dfa38cbaba8217b712755cff831ffdb5f21883b
unknown <stanleychan0507@yahoo.com.hk>
2022-09-17 01:49:08 -0700

    update testPy_2

--- testPy.py
+++ testPy.py
@@ -4,6 +4,10 @@ import individualAssign1
 class TestIndividualAssign1(unittest.TestCase):
 
     def test_randomRank(self):
-        rank = [10, 7, 3]
-        sorted_rank = individualAssign1.sortCard(rank)
+        cardInHandRank = [10, 7]
+        cardInHandSuit = ['Spade', 'Heart']
+        rankSelected = 3
+        suitSelected = 'Club'
+        
+        sorted_rank = individualAssign1.sortRank(cardInHandRank, cardInHandSuit, rankSelected, suitSelected)
         self.assertEqual(sorted_rank, [3, 7, 10])
