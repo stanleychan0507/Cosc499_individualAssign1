8bb07f5ac9df3c33505f4c440d9378b6ddceee5d
unknown <stanleychan0507@yahoo.com.hk>
2022-09-17 00:46:42 -0700

    update individual_1, update testPy_1

--- testPy.py
+++ testPy.py
@@ -1,8 +1,9 @@
 import unittest
+import individualAssign1
 
 class TestIndividualAssign1(unittest.TestCase):
 
     def test_randomRank(self):
         rank = [10, 7, 3]
-        sorted_rank = self(rank)
+        sorted_rank = individualAssign1.sortCard(rank)
         self.assertEqual(sorted_rank, [3, 7, 10])
